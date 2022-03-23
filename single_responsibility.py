5c6b596709da83d64e09e0c00e3fb0cb9f209346
Hov007 <hovayvazyan7@gmail.com>
2022-03-23 17:21:39 +0400

    single_responsibility(not finished)

---
+++ single_responsibility.py
@@ -0,0 +1,22 @@
+from solid import *
+
+
+class PaymentProcessor:
+    def pay_debit(self, order, security_code):
+        print("Processing debit payment type")
+        print(f"Verifying security code: {security_code}")
+        order.status = "paid"
+
+    def pay_credit(self, order, security_code):
+        print("Processing credit payment type")
+        print(f"Verifying security code: {security_code}")
+        order.status = "paid"
+
+
+order = Order()
+order.add_item("Keyboard", 1, 50)
+order.add_item("SSD", 1, 150)
+order.add_item("USB Cable", 2, 5)
+
+print(order.total_price())
+order.pay("debit", "0372846")
---
+++ solid.py
@@ -0,0 +1,16 @@
+class Order:
+    items = []
+    quantities = []
+    prices = []
+    status = "open"
+
+    def add_item(self, name, quantity, price):
+        self.items.append(name)
+        self.quantities.append(quantity)
+        self.prices.append(price)
+
+    def total_price(self):
+        total = 0
+        for i in range(len(self.prices)):
+            total += self.quantities[i] * self.prices[i]
+        return total
